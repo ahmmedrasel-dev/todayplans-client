322e189e3b38c32c5f3ccc675bdfde5e0679d98c
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-18 22:18:25 +0300

    project instal and setup

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Home.</h1>
+      <button class="btn btn-primary">Home</button>
+
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>this is login</h1>
+      <button class="btn btn-primary">Login</button>
+
+    </div>
+  );
+};
+
+export default Login;
