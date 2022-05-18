992334c2ebe7d6f4486c1cd5ca1a3d5f0ea0cb4e
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-19 01:00:46 +0300

    dispay todo list

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,39 @@
+import axios from 'axios';
 import React from 'react';
 import TodoList from '../TodoList/TodoList';
 
 const Home = () => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const todoTask = {
+      name: e.target.task.value,
+      description: e.target.description.value
+    }
+    const url = `http://localhost:5000/add-task`;
+    try {
+      const postTask = async () => {
+        const response = await axios.post(url, todoTask);
+      }
+      postTask();
+
+
+    }
+    catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
     <div className='w-3/5 mx-auto mt-5'>
       <div className="card w-full bg-sky-100 shadow-xl h-screen">
         <div className="card-body">
           <h2 className="text-center text-2xl">What's the Plan for Today!</h2>
           <div className="card-actions justify-center">
-            <form className='w-full flex justify-center'>
-              <div class="form-control w-full flex flex-row justify-center">
-                <input type="text" placeholder="Task Name" class="input input-bordered w-40" />
-                <input type="text" placeholder="Task Description" class="input input-bordered w-96 mx-2" />
-                <button class="btn btn-active btn-primary">Add Task</button>
+            <form onSubmit={handleSubmit} className='w-full flex justify-center'>
+              <div className="form-control w-full flex flex-row justify-center">
+                <input type="text" name="task" placeholder="Task Name" className="input input-bordered w-40" />
+                <input type="text" name="description" placeholder="Task Description" className="input input-bordered w-96 mx-2" />
+                <button className="btn btn-active btn-primary">Add Task</button>
               </div>
             </form>
           </div>
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/task`;
+    const getTask = async () => {
+      const { data } = await axios.get(url);
+      setTasks(data);
+    }
+    getTask();
+  }, [tasks])
   return (
-    <div>
-      <h1>Here is the list.</h1>
+    <div className="overflow-x-auto mt-4">
+      <table className="table w-full">
+        <tbody>
+          {
+            tasks.map((task, index) => <tr key={task._id} style={{ marginBottom: '20px' }}>
+              <th>{index + 1}</th>
+              <td>{task.name}</td>
+              <td>{task.description}</td>
+              <td className='w-20'><button className="btn bg-green-500 m-1">Complete</button>
+                <button className="btn bg-red-500 m-1">Delete</button></td>
+            </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
