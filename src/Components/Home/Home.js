8edfda7ce72b3894bf0f4f203a4364347e9bdaa7
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-18 22:56:25 +0300

    add form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import TodoList from '../TodoList/TodoList';
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is Home.</h1>
-      <button class="btn btn-primary">Home</button>
-
+    <div className='w-3/5 mx-auto mt-5'>
+      <div className="card w-full bg-sky-100 shadow-xl h-screen">
+        <div className="card-body">
+          <h2 className="text-center text-2xl">What's the Plan for Today!</h2>
+          <div className="card-actions justify-center">
+            <form className='w-full flex justify-center'>
+              <div class="form-control w-full flex flex-row justify-center">
+                <input type="text" placeholder="Task Name" class="input input-bordered w-40" />
+                <input type="text" placeholder="Task Description" class="input input-bordered w-96 mx-2" />
+                <button class="btn btn-active btn-primary">Add Task</button>
+              </div>
+            </form>
+          </div>
+          <TodoList></TodoList>
+        </div>
+      </div>
     </div>
   );
 };
